6ae364117aab41e76d2a4cd31ddd203a7c153caf
Iza <izabelajilavu@gmail.com>
2025-09-12 12:52:47 +0300

    cercetari section

--- assets/js/contentful-service.js
+++ assets/js/contentful-service.js
@@ -397,3 +397,37 @@ export async function fetchSpecializationsData() {
         return { categories: [], specializations: [] };
     }
 }
+
+export async function fetchCercetariFromContentful() {
+    await initializeContentful();
+    if (!client) return {};
+
+    try {
+        const response = await client.getEntries({
+            content_type: 'articolCercetareStiintifica',
+            include: 1
+        });
+
+        if (!response.items) return {};
+
+        const articles = response.items.reduce((acc, item) => {
+            const article = item.fields;
+            if (article.titlu && article.slug) {
+                acc[article.slug] = {
+                    title: article.titlu,
+                    slug: article.slug,
+                    content: article.continutArticol
+                };
+            }
+            return acc;
+        }, {});
+
+        console.log(articles)
+        
+        return articles;
+
+    } catch (error) {
+        console.error('Error fetching cercetari articles from Contentful:', error);
+        return {};
+    }
+}
