f4a2dd25e287f547dbf788cea45522b88e8b539f
Iza <izabelajilavu@gmail.com>
2026-01-24 12:36:02 +0200

    ui fixes

--- assets/js/team.js
+++ assets/js/team.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
                 infinite: false,
                 speed: 300,
                 slidesToShow: 3,
-                slidesToScroll: 1,
+                slidesToScroll: 3,
                 arrows: true,
                 responsive: [
                     {
